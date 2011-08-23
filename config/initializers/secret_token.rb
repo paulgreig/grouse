8c68abe8388e9dc78de6b80b235bfdc02ce12a1b
Paul Greig <paulgreig@home.lan>
2011-08-24 05:03:49 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grouse::Application.config.secret_token = 'c4906c26d60a8f787ba85898f91e411f49b1a5208c240baceae7beff855ed33ef56244001ba04337afc953d83b96aaa8581f8de6613c087ee819f04809c4371e'
