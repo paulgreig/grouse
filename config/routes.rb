6a3e33a932d975d1b154160d6261a6af773c2491
Paul Greig <paulgreig@Macintosh.local>
2011-08-25 21:20:51 +1000

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Grouse::Application.routes.draw do
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
   get "pages/home"
 
   get "pages/contact"
   
   get "pages/about"
+
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
