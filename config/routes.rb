173f17cb9dc4c83300eddf255288d8a7c7dbbb48
Paul Greig <paulgreig@home.lan>
2011-09-11 12:11:17 +1000

    finished layout an routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
+#Grouse::Application.routes.draw do
+
+  #get "users/new"
+
+ # get "pages/home"
+  #get "pages/contact"
+  #get "pages/about"
 Grouse::Application.routes.draw do
-  
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-  
+  get "users/new"
+
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
   root :to => 'pages#home'
-  get "pages/home"
-
-  get "pages/contact"
-  
-  get "pages/about"
-
   
   
 
